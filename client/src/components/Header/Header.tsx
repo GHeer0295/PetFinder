ea56577cfca607468bf89872b578fdeeaf6cc3eb
jdn8 <jack_nguyen@sfu.ca>
2024-03-12 08:04:00 -0700

    Edit spacing for Header.tsx

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from '../../resources/logo.png';
-import './Header.css';
-import { IconContext } from 'react-icons';
+import React from "react";
+import logo from "../../resources/logo.png";
+import "./Header.css";
+import { IconContext } from "react-icons";
 import { FaUserCircle } from "react-icons/fa";
 
-
 const Header: React.FC = () => {
-    return (
-        <header className="header-container">
-            <div className="header-content">
-                <img src={logo} alt="Logo" className="logo" />
-                <div className="user-icon">
-                    <IconContext.Provider value={{ color: "black", size: "35px"}}>
-                        <div >
-                            <FaUserCircle/>
-                        </div>
-                    </IconContext.Provider>
-                </div>
+  return (
+    <header className="header-container">
+      <div className="header-content">
+        <img src={logo} alt="Logo" className="logo" />
+        <div className="user-icon">
+          <IconContext.Provider value={{ color: "black", size: "35px" }}>
+            <div>
+              <FaUserCircle />
             </div>
-        </header>
-    );
-}
+          </IconContext.Provider>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
