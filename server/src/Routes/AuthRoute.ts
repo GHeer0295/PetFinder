64487f168b8b08dab71b50f8c8b3c69c60ecf5a1
Kyle Locquiao <klocquiao45@gmail.com>
2024-03-12 23:59:38 -0700

    Setup docker yml for local postgres testing, fix routing for logout

--- server/src/Routes/AuthRoute.ts
+++ server/src/Routes/AuthRoute.ts
@@ -5,4 +5,4 @@ export const authRouter: Router = express.Router();
 
 authRouter.post('/login', login);
 authRouter.post('/register', register);
-authRouter.get('/logout', logout);
+authRouter.post('/logout', logout);
